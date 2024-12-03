a8fe0bee83962f16224cd1918d6b6dcf0c51f056
Sean Kelly <sean.kelly@grindr.com>
2024-12-02 23:40:24 -0800

    day3 p2 + cleanup

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -4,32 +4,49 @@ struct Day03: AdventDay {
   // Save your data in a corresponding text file in the `Data` directory.
   var data: String
   
-  // Splits input data into its component parts and convert from string.
-  var entities: [[Int]] {
-    data.split(separator: "\n\n").map {
-      $0.split(separator: "\n").compactMap { Int($0) }
+  // Replace this with your solution for the first part of the day's challenge.
+  func part1() -> Int {
+    let products = matches(in: data).compactMap { match in
+      product(from: match)
     }
+    
+    return products.reduce(0, +)
   }
   
-  // Replace this with your solution for the first part of the day's challenge.
-  func part1() -> Any {
+  func matches(in string: String) -> [Regex<Regex<Substring>.RegexOutput>.Match] {
     let regex = /mul\(\d+,\d+\)/
     
-    let mul = data.matches(of: regex).map { match in
-      let match = match.localizedLowercase
-      let digits = match.split(separator: ",")
-      let first = Int(digits.first?.trimmingPrefix("mul(") ?? "") ?? 0
-      let last = Int(digits.last?.trimmingSuffix(while: { $0 == ")" }) ?? "") ?? 0
-      
-      return first * last
-    }
+    return string.matches(of: regex)
+  }
+  
+  private func product(from match:  Regex<Regex<Substring>.RegexOutput>.Match) -> Int? {
+    guard let match = String(match.utf8) else { return nil }
+    let digits = match.split(separator: ",")
+    guard let firstDigit = digits.first?.trimmingPrefix("mul("),
+          let lastDigit = digits.last?.trimmingSuffix(while: { $0 == ")" }) else { return nil }
     
-    return mul.reduce(0, +)
+    guard let first = Int(firstDigit), let last = Int(lastDigit) else { return nil }
+
+    return first * last
   }
   
   // Replace this with your solution for the second part of the day's challenge.
-  func part2() -> Any {
-    // Sum the maximum entries in each set of data
-    entities.map { $0.max() ?? 0 }.reduce(0, +)
+  func part2() -> Int {
+    /// splitting on do() first marks all products as valid until we find the next dont() in the line
+    let enabledLines = data.split(separator: "do()").map {
+      $0.split(separator: "don't()").first
+    }
+
+    var total = 0
+    for enabledSubstring in enabledLines {
+      guard let enabledSubstring else { continue }
+      let enabled = String(enabledSubstring)
+      let products = matches(in: enabled).map { match in
+        product(from: match)!
+      }
+      total += products.reduce(0, +)
+    }
+    
+    return total
   }
 }
